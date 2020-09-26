14878780f8e5f047ccaa57839c9ead062e591d88
DESKTOP-AOSNOAI\User <zuborev_artem@mail.ru>
2020-09-26 20:51:15 +0300

    add code dream-team

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,18 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
+module.exports = function createDreamTeam(members) {
+  if (!Array.isArray(members)) {
+    return false;
+  }
+  let name = '';
+  for (let i = 0; i < members.length; i++) {
+    if (typeof members[i] === 'string') {
+      members[i] = members[i].trim().split('');
+      name += members[i][0].toUpperCase();
+    }
+  }
+  return name.split('').sort().join('');
+
+  //throw new CustomError('Not implemented');
   // remove line with error and write your code here
 };
