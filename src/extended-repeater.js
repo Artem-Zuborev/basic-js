0671c77a614d880313e2ef5a406df923f0a3f85f
DESKTOP-AOSNOAI\User <zuborev_artem@mail.ru>
2020-10-16 20:46:30 +0300

    add

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,30 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function repeater(str, options) {
+  let result = ''
+  if (options.separator === undefined) {
+    options.separator = '+'
+  }
+  if (options.additionSeparator === undefined) {
+    options.additionSeparator = '|'
+  }
+  if (options.repeatTimes === 0 || options.repeatTimes === undefined) {
+    result += str
+    if (options.additionRepeatTimes === 0 || options.additionRepeatTimes === undefined) {
+      result += options.addition
+    }
+  }
+  for (let i = 0; i < options.repeatTimes; i++) {
+    result += str
+    for (let j = 0; j < options.additionRepeatTimes; j++) {
+      result += options.addition
+      if (j < options.additionRepeatTimes - 1) {
+        result += options.additionSeparator
+      }
+    }
+    if (i < options.repeatTimes - 1) {
+      result += options.separator
+    }
+  }
+  return result
 };
-  
